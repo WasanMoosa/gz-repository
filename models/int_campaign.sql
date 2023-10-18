fb179466f7b3b3237c40a923fa9ad68b4497dc60
Wasan AlRiyami <wasssoon@gmail.com>
2023-10-18 12:30:12 +0000

    finish

---
+++ models/int_campaign.sql
@@ -0,0 +1,13 @@
+{{ config(schema='campaigns') }}
+select *
+from {{ ref("stg_adwords") }}
+union all
+select *
+from {{ ref("stg_bing") }}
+union all
+select *
+from {{ ref("stg_criteo") }}
+union all
+select *
+from {{ ref("stg_facebook") }}
+
